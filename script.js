d44e970d287172bee3882e340708226851ee7864
Serko <atmaca.serkan.sio@gmail.com>
2024-04-16 19:32:06 +0200

    add baffle

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 function generate_pass() {
 
-    //Get HTML element
-
   const span_pass = document.getElementById("password");
   const length = parseInt(document.getElementById("length_pass").value);
-
-  // variable declaration
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!:;,*ù$^=)àç_è-(é&";
-    let password = "";
-
-
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!:;,*ù$^=)àç_è-(é&";
+  let password = "";
 
   if (isNaN(length) || length === 0) {
-    alert("La longueur doit être un nombre supérieur à zéro.");
-    return;
+      alert("La longueur doit être un nombre supérieur à zéro.");
+      return;
   }
- 
-  // generate the password
+
   for (let i = 0; i < length; i++) {
-    password += chars.charAt(Math.floor(Math.random() * chars.length));
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
   }
 
   span_pass.innerHTML = password;
 
-  return password;
+
+      let b = baffle('.password'); 
+      b.start();
+      b.set({
+        speed: 100,
+        characters: '█<▓ ░█▒█▒ ░▒/░█ ▓▒▓ █/██▒ █▓█/ █▒█ ░█▒█ ▓░█▒'
+    });
+      b.text(() => password);
+      b.reveal(1500); 
+
+
 }
